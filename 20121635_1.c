7335a9382016d33b9551ba6383fde38ceda07302
JS.Jang <whdtjr321@gmail.com>
2018-04-15 20:55:15 +0900

    mode 4 init

--- 20121635_1.c
+++ 20121635_1.c
@@ -53,6 +53,13 @@ int changeToOcta(int);
 void makeString(int* , int , char );
 void writeString(int *, unsigned char* );
 void countPush(int *shm_addr, int pushcount);
+void inverseDot(int* shm_addr);
+
+
+void makeDotAry(int* shm_addr, char fpga[10]);
+void refreshDot(int* shm_addr);
+
+int ppow(int num, int mul);
 
 
 typedef struct __cursor {
@@ -204,6 +211,8 @@ int output_process(int shm_id)
     unsigned char retval;
     unsigned char buzzData;
 
+    char fpga_data[10];
+
     int fd, rd, mode;
     int exit_flag = 0;
      //for fnd device
@@ -215,6 +224,8 @@ int output_process(int shm_id)
     int dev_buzzer;
 
 	int set_num;
+
+    int i, j;
 
     printf("output process %d started\n", getpid());
 
@@ -404,6 +415,53 @@ int output_process(int shm_id)
 
                 break;
             case 4:
+
+
+                lednum = 0;
+                retval = write(dev_led, &lednum, 1);
+                if(retval < 0) {
+                    printf("LED init error..\n");
+                    return -1;
+                }
+                retval = write(dev_dot_font, fpga_number[2], sizeof(fpga_number[2]));
+                if(retval < 0) {
+                    printf("DOT init error..\n");
+                    return -1;
+                }
+                //init text lcd
+                memset(ledtext, 0x00, MAX_STRING);
+
+                retval = write(dev_text_lcd, ledtext, MAX_STRING);
+
+
+
+                initData(fpga_data);
+
+                //write precedent image
+                makeDotAry(shm_addr, fpga_data);
+
+
+                /////write cursor
+                row = shm_addr[42];
+                col = shm_addr[43];
+
+                if(shm_addr[44] % 2 == 0 && shm_addr[makeIdx(row, col)] == 0 && shm_addr[115] == 0) {
+                    fpga_data[0][col] += ppow(2, row);
+                    retval = write(dev_dot_font, &fpga_data, sizeof(fpga_data));
+                }
+
+                write(dev_dot_font, &fpga_data, sizeof(fpga_data));
+
+                //write fnd...
+                fnd_data[0] = shm_addr[1];
+                fnd_data[1] = shm_addr[2];
+                fnd_data[2] = shm_addr[3];
+                fnd_data[3] = shm_addr[4];
+                retval = write(dev_fnd, &fnd_data, 4);
+                if(retval < 0) {
+                    printf("FND write error..\n");
+                    return -1;
+                }
                 break;
             case 5:
                 break;
@@ -754,6 +812,96 @@ int main_process(int shm_id)
 
 				break;
             case 4:
+
+
+                read(dev_switch, &push_sw_buff, buff_size);
+
+                //initialize
+                for(i = 0 ; i < 9 ; i++) {
+                    idxCount[i] = 0;
+                }
+                curStrnum = 0;
+                pushcount = 0;
+                buttcount = 0;
+                textMode = 0;
+                preval = -1;
+                premode = -1;
+                /////get time
+                time(&ctime);
+                tm=localtime(&ctime);
+
+                shm_addr[44] = tm->tm_sec;
+                ///move cursor...
+
+                for(i = 0 ; i < 9 ; i++) {
+                    if(push_sw_buff[i] == 1)
+                    buttcount++;
+                }
+
+                if(buttcount > 1)  //valid for one button input
+                    continue;
+                else if(push_sw_buff[1] == 1) {  //go up
+                    printf("cursor go up!!..\n");
+                    if(curdot.col > 0)
+                    curdot.col--;
+                }
+                else if(push_sw_buff[3] == 1) {
+                    printf("cursor go left!!..\n"); //go left
+                    if(curdot.row < 6)
+                    curdot.row++;
+                }
+                else if(push_sw_buff[5] == 1) {
+                    printf("cursor go right!!..\n");  //go right
+                    if(curdot.row > 0)
+                    curdot.row--;
+                }
+                else if(push_sw_buff[7] == 1) {  //go down
+                    printf("cursor go down!!..\n");
+                    if(curdot.col < 9) 
+                    curdot.col++;
+                }
+                /////
+                if(buttcount > 0)
+                    pushcount2++;
+
+                shm_addr[42] = curdot.row;
+                shm_addr[43] = curdot.col;
+
+                ///// coloring dot
+                if(push_sw_buff[0] == 1) {   //reset all setting
+                    refreshDot(shm_addr);
+                    curdot.row = 6;
+                    curdot.col = 0;
+                    shm_addr[42] = curdot.row;
+                    shm_addr[43] = curdot.col;
+                    shm_addr[115] = 0;
+                    pushcount2 = 0;
+                }
+                else if(push_sw_buff[2] == 1) { //cursor on / off
+                    shm_addr[115] = 1 - shm_addr[115];
+                }
+                else if(push_sw_buff[4] == 1) {
+                    shm_addr[makeIdx(curdot.row, curdot.col)] = 1;
+                }
+                else if(push_sw_buff[6] == 1) { //refresh Dot
+                    refreshDot(shm_addr);	
+                }
+                else if(push_sw_buff[8] == 1) {
+                    inverseDot(shm_addr);
+                }
+                //fnd counter
+                countPush(shm_addr, pushcount2);
+
+
+
+
+
+
+
+
+
+
+
 				break;
             case 5:
 				break;
@@ -806,15 +954,6 @@ void countPush(int *shm_addr, int pushcount){
 	shm_addr[4] = pushcount%10;
 }
 
-void initDotshm(int* shm_addr) //initialize dot data
-{   
-  int i;
-  shm_addr[42] = 6;
-  shm_addr[43] = 0;
-  for(i = 45 ; i < 115 ; i++) {
-    shm_addr[i] =0;
-  }
-}
 
 
 
@@ -844,6 +983,63 @@ int changeToTwo(int num)
     }
         
 	return changeToTwo(num/2)*10 + num%2;
+
+}
+
+
+void initDotshm(int* shm_addr) //initialize dot data
+{   
+  int i;
+  shm_addr[42] = 6;
+  shm_addr[43] = 0;
+  for(i = 45 ; i < 115 ; i++) {
+    shm_addr[i] =0;
+  }
+}
+
+
+
+void makeDotAry(int* shm_addr, char fpga[10]) {    //make dot data by using shared memory data
+  int i, j;
+  int sum;
+  for(i = 0 ; i < 10 ; i++) {
+    sum = 0;
+    for(j = 45 + i ; j < 106 + i ; j = j + 10) {
+      sum = sum + shm_addr[j] * ppow(2, (j - 45 - i)/10);
+    }
+    fpga[i] += sum;
+  }
+}
+
+
+void refreshDot(int* shm_addr) {  //refresh dot array
+  int i, j;
+  for(i = 0 ; i < 10 ; i++) {
+    for(j = 45 + i ; j < 106 + i ; j = j + 10) {
+      shm_addr[j] = 0;
+    }
+  }
+
+}
+
+void inverseDot(int* shm_addr) {   //inverse dot data
+  int i, j;
+  for(i = 0 ; i < 10 ; i++) {
+    for(j = 45 + i ; j < 106 + i ; j = j + 10) {
+      shm_addr[j] = 1 - shm_addr[j];
+    }
+  }
+}
+
+
+
+int ppow(int num, int mul) {  //same as pow in math.h
+  int i;
+  int ret = 1;
+  for(i = 0 ; i < mul ; i++) {
+    ret = ret*num;
+  }
+  return ret;
 
 }
 
